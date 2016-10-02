60d7feed520a9f90995894d9b2d237db6e463fb0
Micah Picasso <bobermen1@localhost.localdomain>
2016-10-02 11:12:19 -0600

    Contains functions to draw the different spirals

---
+++ DrawSpiral.cpp
@@ -0,0 +1,5 @@
+#include "DrawSpiral.h"
+
+using namespace std;
+
+void drawSquareSpiral();
---
+++ DrawSpiral.h
@@ -0,0 +1,13 @@
+#ifndef _DrawSpiral_h_
+#define _DrawSpiral_h_
+
+#include <vector>
+#include <cmath>
+#include <GL/freeglut.h>
+#include "Globals.h"
+
+using namespace std;
+
+void drawSquareSpiral();
+
+#endif
