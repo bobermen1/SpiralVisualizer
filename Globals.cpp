eff03d13d8bcab444fe6ed965818bd30c10438f0
Micah Picasso <bobermen1@localhost.localdomain>
2016-10-02 10:38:16 -0600

    File for storing global constants and globals used by multiple files

---
+++ Globals.cpp
@@ -0,0 +1,6 @@
+#include "Globals.h"
+
+using namespace std;
+
+int ScreenWidth = 800;
+int ScreenHeight = 800;
---
+++ Globals.h
@@ -0,0 +1,13 @@
+#ifndef _Globals_h_
+#define _Globals_h_
+
+#include <vector>
+#include <cmath>
+#include <GL/freeglut.h>
+
+using namespace std;
+
+extern int ScreenWidth;
+extern int ScreenHeight;
+
+#endif
